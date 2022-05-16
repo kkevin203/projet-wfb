650ebe0f93bf8d99c09c658d87dad43e18456951
kkevin203 <binokev@hotmail.com>
2022-05-16 11:38:16 +0200

    update js

--- front/head.php
+++ front/head.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="design/default.css">
+
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;1,300&family=Roboto:wght@100&display=swap" rel="stylesheet">
     <!--bootstrap CSS-->
     <link rel="stylesheet" href="<?php echo (CSSPATH . "$cssBootstrap"); ?>" type="text/css">
-    <!-- <script src="./script/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-    <!-- <script src="config.php?script1=./script/bootstrap.bundle.min.js"></script> -->
+    <script src="./front/script/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="design/default.css">
     <!-- propriétés de la pages  -->
     <title>Les New's de la WFB</title>
 </head>
