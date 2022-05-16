618d26b3b12e7bac05e09aadbdc5fc780b1e45cb
kkevin203 <binokev@hotmail.com>
2022-05-16 09:37:08 +0200

    update Twitch

--- Twitch.php
+++ Twitch.php
@@ -8,64 +8,25 @@ require_once 'front/head.php'
 
 <body>
 	<!-- entetes -->
-	<header>
-		<table>
-			<tr>
-				<td>
-					<!-- logo -->
-					<img src="pictures/logowfb.png" alt="logo" title="ceci est notre logo" id="logo" />
-				</td>
-				<td width=50%>
-					<h1>Wolfblues Esport</h1>
-					<!-- statut -->
-					<h2><u>Association de jeu vidéo esport</u></h2>
-				</td>
-			</tr>
-		</table>
 
-		<!-- menu top  -->
-		<nav>
-			<div id="conexion">
-				<ul>
-					<li>Devenir membre</li>
-					<li>conexion</li>
-					<li>Facebook</li>
-					<li>Twitter</li>
-				</ul>
-			</div>
-		</nav>
-
-	</header>
-
-
-
-	<!-- menu -->
-	<nav id="menu">
-		<ul>
-			<li><a href="acceuil.html">Acceuil</a></li>
-			<li><a href="presentation.html">Qui sommes nous ?</a></li>
-			<li><a href="news.html">news</a></li>
-			<li><a href="partenaire.html">Partenaire</a></li>
-			<li><a href="twitch.html"> chaine twitch</a></li>
-		</ul>
-	</nav>
-	</head>
+	<!-- entetes -->
+	<?php
+	// header
+	require_once 'front/header.php';
+	// navigation connection
+	require_once 'front/nav-top.php';
+	// navigation menu 
+	require_once 'front/nav.php';
+	?>
 
 	<body>
 
 	</body>
 	<!-- contact -->
-	<section>
-		<h3>Contact</h3>
-		<div id="contact">
-			<!-- description -->
-			<p>Si vous voulez nous contactez hesitez pas a nous envoyer un email a <a href="mailto:Wolfblue.contact@gmail.com"><b>Wolfblue.contact@gmail.com</b><br />
-					<b>adresse</b>: 47 Rue des Vanneaux, 72100 Le Mans, France<br />
-					<b>Téléphone</b>: 0777269463
-				</a></p>
-		</div>
-
-	</section>
+	<?php
+	require_once 'front/contact.php';
+	require_once 'front/footer.php';
+	?>
 
 
 </html>
